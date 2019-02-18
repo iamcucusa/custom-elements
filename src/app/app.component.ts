fcdb824fd9b2f9925435e60a3184d8be7e19c28f
Henriquez Fernandez <GracielaHen@youforce.net>
2019-02-18 16:26:06 +0100

    create zoom button component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'zoom-button',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'custom-elements';
+
+  title = 'Agnostic component example for Zoom design system';
+  count: number = 0;
+
+  @Input() mode: string;
+
+  @Input() raetId: string;
+
+  @Input() size: string;
+
+  @Input() type: string;
+
+  @Input() isDisabled: boolean;
+
+  @Output() actionClick: EventEmitter<number> = new EventEmitter();
+
+  isLoading = false;
+
+  isActive = false;
+
+  constructor(private changeDetectorRef: ChangeDetectorRef) {
+  }
+
+  showLoading() {
+    this.isLoading = true;
+  }
+
+  hideLoading() {
+    this.isLoading = false;
+  }
+
+  showActive() {
+    this.isActive = true;
+  }
+
+  hideActive() {
+    this.isActive = false;
+  }
+
+  onClick() {
+    this.count++;
+    this.actionClick.emit(this.count);
+    this.changeDetectorRef.detectChanges();
+  }
+
+  done() {
+    this.isLoading = false;
+    this.isActive = false;
+  }
+
 }
